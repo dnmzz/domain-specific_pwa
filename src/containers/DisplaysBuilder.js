e4e44cae046e4deac6912f4bf0f7480976fedd4f
Diogo <diogosampaio89@gmail.com>
2020-02-26 23:02:27 +0000

    adding some components and basic routing

--- src/containers/DisplaysBuilder.js
+++ src/containers/DisplaysBuilder.js
@@ -1,19 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Display from '../components/Display/Display';
-import { display } from '../display_mockData'
-
-
-
-
+import { display } from '../display_mockData';
+import NavBar from '../components/NavBar';
+import axios from "axios";
 
 const DisplaysBuilder = () => {
 
     const [displayState, setDisplayState] = useState(display);
 
+    // useEffect(() => {
+    //     axios
+    //       .get("https://jsonplaceholder.typicode.com/users")
+    //       .then(response => setDisplayState(response.data));
+    //   }, []);
 
-    displayState.map((display, index) => {
-        return <Display name={display[index].name} />
-    })
+
+    var displays = displayState.map(display => {
+        return (
+            <Display name={display.name} />
+        );
+    });
+
+    return <div>
+        {/* <NavBar /> */}
+        {displays}
+    </div>
+
+    // return (<Display name={displayState[0].name} />)
+    // displayState.map((display, index) => {
+    //     return <Display name={display[index].id} />
+    // })
 }
 
 export default DisplaysBuilder;
