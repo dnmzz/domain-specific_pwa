79fec6e49b856b25afd796412ff3a96437dd2ae5
Diogo <diogosampaio89@gmail.com>
2020-03-30 20:46:09 +0100

    remove unnused imports

--- src/components/Display/Display.js
+++ src/components/Display/Display.js
@@ -3,18 +3,11 @@ import { makeStyles } from '@material-ui/core/styles';
 import 'typeface-roboto';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import CardText from '@material-ui/core/Card';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
-import TimerIcon from '@material-ui/icons/Timer';
-import ShareIcon from '@material-ui/icons/Share';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
 import { useHistory } from "react-router-dom";
 import useBoundingclientrect from "@rooks/use-boundingclientrect"
 import Iframe from 'react-iframe';
