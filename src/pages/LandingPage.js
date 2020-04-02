b63b0badd82d81d7ba7d2e2d60987d4f45d4e208
Diogo <diogosampaio89@gmail.com>
2020-04-02 01:05:22 +0100

    adding landing page and css corrections

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -3,9 +3,6 @@ import { useHistory } from "react-router-dom";
 import Typical from 'react-typical'
 import { makeStyles } from '@material-ui/core/styles';
 import Fab from '@material-ui/core/Fab';
-import AddIcon from '@material-ui/icons/Add';
-import EditIcon from '@material-ui/icons/Edit';
-import FavoriteIcon from '@material-ui/icons/Favorite';
 import NavigationIcon from '@material-ui/icons/Navigation';
 
 import './landingPage.css';
@@ -31,7 +28,6 @@ function LandingPage(props) {
         });
     };
     return (
-
         <div className={classes.root}>
             <div className="landingPage-entryText">
                 <h1>A MODERN WAY TO</h1>
